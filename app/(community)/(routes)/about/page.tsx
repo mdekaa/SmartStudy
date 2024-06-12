698cd1e6e59056d653a44a509aecad2f2d0e8b7f
Khabir Ahmed <khabirahmed780@gmail.com>
2024-06-13 01:22:10 +0530

    about us page

--- app/(community)/(routes)/about/page.tsx
+++ app/(community)/(routes)/about/page.tsx
@@ -1,10 +1,56 @@
 "use client"
 
 import React from 'react'
+import LandingNavbar from '@/components/landing-navbar'
+import AboutContent from '@/components/about-content'
 
 const about = () => {
   return (
-    <div className="text-white font-bold py-20 text-center space-y-5 bg-gradient-to-r from-slate-600 to-slate-900">about</div>
+    <div className="h-full">
+      <LandingNavbar />
+      <div className="text-white font-bold py-10 pt-10 text-center space-y-5 bg-gradient-to-r from-slate-600 to-slate-900">
+        <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+          <h1 >About Us</h1>
+
+          <p className="text-lg">Welcome to SmartStudy, your trusted companion for academic success!</p>
+          <p className="text-lg">
+            At SmartStudy, we believe that every student deserves the best tools to achieve their academic goals. Our mission is to provide an intuitive and accurate platform that helps students project their CGPA (Cumulative Grade Point Average) and plan their academic journey with confidence.
+          </p>
+        </div>
+      </div>
+
+      <div className="text-white ">
+        <AboutContent />
+
+      </div>
+      <div className="text-white font-bold py-36 pt-10 text-center space-y-5 bg-gradient-to-r from-slate-600 to-slate-900">
+
+        <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+          <h2 className="text-center text-4xl text-white font-extrabold">
+            Our Story
+          </h2>
+          <p className="text-lg">
+            SmartStudy was founded by a group of passionate sudents and tech enthusiasts who recognized the need for a reliable and user-friendly tool to assist students in their academic journey. We combined our expertise in education, data analysis, and software development to create a platform that empowers students to make informed decisions about their studies.
+          </p>
+        </div>
+        <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+          <h2 className="text-center text-4xl text-white font-extrabold">
+            Join Our Community
+          </h2>
+          <p className="text-lg">
+            Join thousands of students who are already using CGPA Predictor to enhance their academic performance. Whether you're aiming for the top of your class or just trying to stay on track, we're here to help you every step of the way.
+          </p>
+        </div>
+        <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
+          <h2 className="text-center text-4xl text-white font-extrabold">
+            Contact Us
+          </h2>
+          <p className="text-lg">
+            Have any questions or feedback? We'd love to hear from you! Reach out to us at support@smartstudy.com or follow us on social media for the latest updates and tips.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ components/about-content.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from "react";
+import { cn } from "@/lib/utils";
+
+
+import {
+    
+    Percent,
+    Handshake,
+    Lightbulb,
+    Goal
+
+  } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+
+const offers = [
+  {
+    name: "Accurate Predictions",
+    avatar: Percent,
+    description: "Using advanced algorithms, we offer precise CGPA predictions based on your current grades and future academic plans."
+  },
+  {
+    name: "User-Friendly Interface",
+    avatar: Handshake,
+    description: "Our platform is designed to be easy to use, ensuring that students of all technical backgrounds can navigate and utilize our tools effectively"
+  },
+  {
+    name: "Comprehensive Insights",
+    avatar: Lightbulb,
+    description:"Beyond predictions, we provide detailed insights and analysis to help you understand the factors impacting your academic performance"
+  },
+  {
+    name: "Goal Setting",
+    avatar: Goal,
+    description:"Set your academic targets and track your progress towards achieving them. Our platform offers personalized tips and strategies to help you stay on track"
+  }
+];
+
+export default function AboutContent() {
+  return (
+    <div className="px-10 pb-20 bg-gradient-to-r from-slate-600 to-slate-900">
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10 p-5">
+        What We Offer
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {offers.map((item) => (
+          <Card
+            key={item.description}
+            className="bg-[#192339] border-none text-white"
+          >
+            <CardHeader>
+              <div className="flex flex-row items-center">
+                <div className="h-10 w-10 mr-4">
+                    <div className="flex items-center flex-1">
+                    <item.avatar className={cn("h-5 w-5 mr-3")} />
+                    
+                </div>
+                </div>
+                <CardTitle className="flex items-center gap-x-2">
+                  <div>
+                    <p className="text-lg">{item.name}</p>
+                    {/* <p className="text-zinc-400 text-sm">{item.title}</p> */}
+                  </div>
+                </CardTitle>
+              </div>
+              <CardContent className="pt-4 px-0 w-56">
+                {item.description}
+              </CardContent>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
